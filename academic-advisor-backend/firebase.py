18352057e7627be2be4e3378b75a9cf1a2f7396c
miniproj1562 <miniproj1562@gmail.com>
2026-01-23 10:50:36 +0530

    node modules

--- academic-advisor-backend/firebase.py
+++
@@ -1,6 +0,0 @@
-import os
-from firebase_admin import credentials, initialize_app
-
-cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-cred = credentials.Certificate(cred_path)
-initialize_app(cred)
